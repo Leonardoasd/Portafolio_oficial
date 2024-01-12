9ba246f05d04fb84949d1ffc5a5647b91e7b231e
Leonardoasd <126222979+Leonardoasd@users.noreply.github.com>
2024-01-12 04:05:48 -0400

    last update

--- src/Screens/misProyectos.js
+++ src/Screens/misProyectos.js
@@ -6,14 +6,6 @@ import Reloj from "../imagenes/reloj25.png"
 
 export const Proyects = [{
     id: 0,
-    nombre: "Poemas Aleatorios",
-    imagen: Poemas,
-    description: "Esta web es un sistema de JavaScript que permite registrar palabras para luego devolver una de esas palabras de manera aleatoria, similar a una lotería.",
-    linkWeb: "https://leonardoasd.github.io/Proyecto-freeCodeCamp/",
-    linkRepository: "https://github.com/Leonardoasd/Proyecto-freeCodeCamp" 
-},
-{
-    id: 1,
     nombre: "Sistema de Aleatoriedad",
     imagen: Aleatoriedad,
     description: "Esta web es un sistema de JavaScript que permite registrar palabras para luego devolver una de esas palabras de manera aleatoria, similar a una lotería.",
@@ -21,20 +13,28 @@ export const Proyects = [{
     linkRepository: "https://github.com/Leonardoasd/Sistema_de_Aleatoriedad.git"
 },
 {
-    id: 2,
-    nombre: "Drum Machine",
-    imagen: Drum,
-    description: "Esta es una página web que básicamente emula lo que sería una caja de ritmos, con toda la lógica hecha con JavaScript y React.",
-    linkWeb: "https://leonardoasd.github.io/drum-machine/",
-    linkRepository: "https://github.com/Leonardoasd/drum-machine" 
+    id: 1,
+    nombre: "Poemas Aleatorios",
+    imagen: Poemas,
+    description: "Esta web es un sistema de JavaScript que permite registrar palabras para luego devolver una de esas palabras de manera aleatoria, similar a una lotería.",
+    linkWeb: "https://leonardoasd.github.io/Proyecto-freeCodeCamp/",
+    linkRepository: "https://github.com/Leonardoasd/Proyecto-freeCodeCamp" 
 },
 {
-    id: 3,
+    id: 2,
     nombre: "Calculator",
     imagen: Calculator,
     description: "Esta app es una calculadora hecha con JavaScript 100% funcional, formó parte de mis proyectos para obtener mi titulación en freeCodeCamp.",
     linkWeb: "https://leonardoasd.github.io/project-Calculator/",
     linkRepository: "https://github.com/Leonardoasd/project-Calculator"
+},
+{
+    id: 3,
+    nombre: "Drum Machine",
+    imagen: Drum,
+    description: "Esta es una página web que básicamente emula lo que sería una caja de ritmos, con toda la lógica hecha con JavaScript y React.",
+    linkWeb: "https://leonardoasd.github.io/drum-machine/",
+    linkRepository: "https://github.com/Leonardoasd/drum-machine" 
 },
 {
     id: 4,
