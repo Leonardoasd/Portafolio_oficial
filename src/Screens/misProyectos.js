2ea21d945f0b70e7d25e7526189c980a44039eeb
Leonardoasd <126222979+Leonardoasd@users.noreply.github.com>
2024-04-04 03:59:12 -0400

    new proyect added

--- src/Screens/misProyectos.js
+++ src/Screens/misProyectos.js
@@ -2,46 +2,56 @@ import Poemas from "../imagenes/frases-aleatorias.png";
 import Aleatoriedad from "../imagenes/SistemaDeAleatoriedad.png"
 import Drum from "../imagenes/drum-machine.png";
 import Calculator from "../imagenes/calculadora-web.png";
-import Reloj from "../imagenes/reloj25.png"
+import Reloj from "../imagenes/reloj25.png";
+import Portes from "../imagenes/portes-valencia.png"
 
-export const Proyects = [{
-    id: 0,
-    nombre: "Sistema de Aleatoriedad",
-    imagen: Aleatoriedad,
-    description: "Esta web es un sistema de JavaScript que permite registrar palabras para luego devolver una de esas palabras de manera aleatoria, similar a una lotería.",
-    linkWeb: "https://leonardoasd.github.io/sistema-de-aleatoriedad/",
-    linkRepository: "https://github.com/Leonardoasd/Sistema_de_Aleatoriedad.git"
-},
-{
-    id: 1,
-    nombre: "Poemas Aleatorios",
-    imagen: Poemas,
-    description: "Esta web tiene aproximadamente 30 poemas cortos los cuales se muestran de manera aleatoria al presionar el botón 'new poem'.",
-    linkWeb: "https://leonardoasd.github.io/Proyecto-freeCodeCamp/",
-    linkRepository: "https://github.com/Leonardoasd/Proyecto-freeCodeCamp" 
-},
-{
-    id: 2,
-    nombre: "Calculator",
-    imagen: Calculator,
-    description: "Esta app es una calculadora hecha con JavaScript 100% funcional, formó parte de mis proyectos para obtener mi titulación en freeCodeCamp.",
-    linkWeb: "https://leonardoasd.github.io/project-Calculator/",
-    linkRepository: "https://github.com/Leonardoasd/project-Calculator"
-},
-{
-    id: 3,
-    nombre: "Drum Machine",
-    imagen: Drum,
-    description: "Esta es una página web que básicamente emula lo que sería una caja de ritmos, con toda la lógica hecha con JavaScript y React.",
-    linkWeb: "https://leonardoasd.github.io/drum-machine/",
-    linkRepository: "https://github.com/Leonardoasd/drum-machine" 
-},
-{
-    id: 4,
-    nombre: "Reloj 25 + 5",
-    imagen: Reloj,
-    description: "Este es el famoso 'reloj de pomodoro', el cual plantea una dinámica de trabajo que se basa, muy resumidamente, en trabajar 25 minutos, y descanzar 5.",
-    linkWeb: "https://leonardoasd.github.io/clock25/",
-    linkRepository: "https://github.com/Leonardoasd/clock25"
-}
+export const Proyects = [
+    {
+        id: 0,
+        nombre: "Portes Valencia",
+        imagen: Portes,
+        description: "Esta es una página web hecha para una empresa de mudanzas en Valencia España, esta es una réplica mejorada de la ya existente página de dicha empresa",
+        linkWeb: "https://leonardoasd.github.io/Portes-Valencia/",
+        linkRepository: "https://github.com/Leonardoasd/Portes-Valencia" 
+    },
+    {
+        id: 1,
+        nombre: "Sistema de Aleatoriedad",
+        imagen: Aleatoriedad,
+        description: "Esta web es un sistema de JavaScript que permite registrar palabras para luego devolver una de esas palabras de manera aleatoria, similar a una lotería.",
+        linkWeb: "https://leonardoasd.github.io/sistema-de-aleatoriedad/",
+        linkRepository: "https://github.com/Leonardoasd/Sistema_de_Aleatoriedad.git"
+    },
+    {
+        id: 2,
+        nombre: "Poemas Aleatorios",
+        imagen: Poemas,
+        description: "Esta web tiene aproximadamente 30 poemas cortos los cuales se muestran de manera aleatoria al presionar el botón 'new poem'.",
+        linkWeb: "https://leonardoasd.github.io/Proyecto-freeCodeCamp/",
+        linkRepository: "https://github.com/Leonardoasd/Proyecto-freeCodeCamp" 
+    },
+    {
+        id: 3,
+        nombre: "Calculator",
+        imagen: Calculator,
+        description: "Esta app es una calculadora hecha con JavaScript 100% funcional, formó parte de mis proyectos para obtener mi titulación en freeCodeCamp.",
+        linkWeb: "https://leonardoasd.github.io/project-Calculator/",
+        linkRepository: "https://github.com/Leonardoasd/project-Calculator"
+    },
+    {
+        id: 4,
+        nombre: "Drum Machine",
+        imagen: Drum,
+        description: "Esta es una página web que básicamente emula lo que sería una caja de ritmos, con toda la lógica hecha con JavaScript y React.",
+        linkWeb: "https://leonardoasd.github.io/drum-machine/",
+        linkRepository: "https://github.com/Leonardoasd/drum-machine" 
+    },
+    {
+        id: 5,
+        nombre: "Reloj 25 + 5",
+        imagen: Reloj,
+        description: "Este es el famoso 'reloj de pomodoro', el cual plantea una dinámica de trabajo que se basa, muy resumidamente, en trabajar 25 minutos, y descanzar 5.",
+        linkWeb: "https://leonardoasd.github.io/clock25/",
+        linkRepository: "https://github.com/Leonardoasd/clock25"
+    }
 ]
